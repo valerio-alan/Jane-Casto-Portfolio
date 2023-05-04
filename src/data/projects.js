3ccfded1cb7fb6fc55d048cef0de2dca6e4a3d57
Jane-Casto <contact@janecasto.com>
2023-05-04 12:57:03 -0700

    Added school segregation project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,17 @@
 export default [
     {
         title:'Covid-19 in California',
-        description:'Analyzing strange Northern California relationship between infection and mortality rates.',
+        description:'Analyzing a strange relationship between infection and mortality rates in Northern California.',
         img:'project_images/Covid-19 in California.png',
         link:'https://public.tableau.com/app/profile/jane.casto/viz/Covid-19DatainCalifornia/Dashboard1',
         tags:['Tableau', 'Google Sheets']
+    },
+    {
+        title:'Modern School Segregation',
+        description:'Examining racial disparities within schools of the same zip codes in California.',
+        img:'project_images/Racial Skewing Box Plots.jpg',
+        link:'',
+        tags:['R', 'Google Sheets','Policy']
     },
     {
         title:'Coming Soon',
