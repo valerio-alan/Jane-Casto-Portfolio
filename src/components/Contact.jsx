61b256d555a6c86c0d0532bb22b6a48441c19883
valerio-alan <alan@valerio.nyc>
2023-05-08 13:32:20 -0700

    Contact links open in new tab

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
         <div className='section' id='Contact'>
             <div className='section-content section-limiter'>
                 <h2 className='section-title popped'>Contact Me</h2>
-                <p className='contact-text'>To follow my current work, follow me on <a href="https://www.linkedin.com/in/jane-casto-b32153252/">LinkedIn</a> and <a href="https://public.tableau.com/app/profile/jane.casto">Tableau</a>.<br/><br/>
+                <p className='contact-text'>To follow my current work, follow me on <a href="https://www.linkedin.com/in/jane-casto-b32153252/" target="_blank">LinkedIn</a> and <a href="https://public.tableau.com/app/profile/jane.casto" target="_blank">Tableau</a>.<br/><br/>
                 For hiring inquiries, contact me via my email, <a href="mailto:contact@janecasto.com">contact@janecasto.com</a></p>
                 <div className="contact-buttons">
                     <a href="https://www.linkedin.com/in/jane-casto-b32153252/" target="_blank">LinkedIn</a>
