c6693865b7c3d20cfc459ec5f756dd98864d454e
Alan Valerio <dev@alanvalerio.com>
2024-10-19 19:45:11 -0700

    Slight Styling Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,84 +1,84 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 export default function Header(props) {
-	const [currentSection, setCurrentSection] = useState('Home')
+  const [currentSection, setCurrentSection] = useState('Home');
 
-	useEffect(() => {
-		const handleScroll = () => {
-			const header = document.querySelector('header')
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector('header');
 
-			let pos_header = window.pageYOffset + header.offsetHeight
-			const sectionDistances = [...props.sections].map((section) => section.element.offsetTop + section.element.offsetHeight / 1.5 - pos_header)
-			let min = Math.min(...sectionDistances.filter((num) => num > 0))
+      let pos_header = window.pageYOffset + header.offsetHeight;
+      const sectionDistances = [...props.sections].map((section) => section.element.offsetTop + section.element.offsetHeight / 1.5 - pos_header);
+      let min = Math.min(...sectionDistances.filter((num) => num > 0));
 
-			props.sections.forEach((section, i) => {
-				if (min === sectionDistances[i]) {
-					setCurrentSection(section.element.id)
-				}
-			})
-		}
+      props.sections.forEach((section, i) => {
+        if (min === sectionDistances[i]) {
+          setCurrentSection(section.element.id);
+        }
+      });
+    };
 
-		handleScroll()
+    handleScroll();
 
-		window.addEventListener('scroll', handleScroll)
-		return () => {
-			window.removeEventListener('scroll', handleScroll)
-		}
-	}, [props.sections])
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [props.sections]);
 
-	useEffect(() => {
-		const headerRight = document.querySelector('.header-right')
-		const headerRightMobile = document.querySelector('.header-mobile-icon')
+  useEffect(() => {
+    const headerRight = document.querySelector('.header-right');
+    const headerRightMobile = document.querySelector('.header-mobile-icon');
 
-		const rightBoundingBox = headerRight.getBoundingClientRect()
-		const rightMobileBoundingBox = headerRightMobile.getBoundingClientRect()
+    const rightBoundingBox = headerRight.getBoundingClientRect();
+    const rightMobileBoundingBox = headerRightMobile.getBoundingClientRect();
 
-		if (currentSection === 'Home') {
-			headerRight.style.setProperty('right', '50%')
-			headerRightMobile.style.setProperty('right', '50%')
-		} else {
-			headerRight.style.setProperty('right', `${rightBoundingBox.width / 2}px`)
-			headerRightMobile.style.setProperty('right', `${rightMobileBoundingBox.width / 2}px`)
-		}
-	}, [currentSection])
+    if (currentSection === 'Home') {
+      headerRight.style.setProperty('right', '50%');
+      headerRightMobile.style.setProperty('right', '50%');
+    } else {
+      headerRight.style.setProperty('right', `${rightBoundingBox.width / 2}px`);
+      headerRightMobile.style.setProperty('right', `${rightMobileBoundingBox.width / 2}px`);
+    }
+  }, [currentSection]);
 
-	const linkEls = [...props.sections].map((section, i) => {
-		return (
-			<div className='header-link pointer' onClick={section.offsetFunc} key={i}>
-				<h2 className={currentSection === section.title ? 'unselectable current-link' : 'unselectable'}>{section.title}</h2>
-			</div>
-		)
-	})
+  const linkEls = [...props.sections].map((section, i) => {
+    return (
+      <div className='header-link pointer' onClick={section.offsetFunc} key={i}>
+        <h2 className={currentSection === section.title ? 'unselectable current-link' : 'unselectable'}>{section.title}</h2>
+      </div>
+    );
+  });
 
-	return (
-		<div className='header-wrapper'>
-			<header className='popped'>
-				<a
-					className='header-left'
-					href='/'
-					style={{
-						opacity: currentSection === 'Home' ? '0' : '1',
-						pointerEvents: currentSection === 'Home' ? 'none' : 'auto',
-					}}
-				>
-					<h1 className='unselectable text-logo'>Jane Casto</h1>
-				</a>
-				<div className='header-right'>{linkEls}</div>
-				<div className='header-mobile-icon mobile pointer' onClick={!props.navOpen ? props.toggleNav : () => {}}>
-					<div className='header-icon-line' style={{ transform: props.navOpen ? 'translateY(11.5px) rotate(45deg)' : '' }}></div>
-					<div className='header-icon-line' style={{ width: props.navOpen ? '0' : '' }}></div>
-					<div className='header-icon-line' style={{ transform: props.navOpen ? 'translateY(-11.5px) rotate(-45deg)' : '' }}></div>
-				</div>
-			</header>
-			<div
-				className='header-mobile-nav mobile'
-				style={{
-					transform: props.navOpen ? 'translateY(0)' : 'translateY(-100%)',
-					opacity: props.navOpen ? '0.8' : '0',
-				}}
-			>
-				{linkEls}
-			</div>
-		</div>
-	)
+  return (
+    <div className='header-wrapper'>
+      <header className='popped'>
+        <a
+          className='header-left'
+          href='/'
+          style={{
+            opacity: currentSection === 'Home' ? '0' : '1',
+            pointerEvents: currentSection === 'Home' ? 'none' : 'auto',
+          }}
+        >
+          <h1 className='unselectable text-logo unbold'>Jane Casto</h1>
+        </a>
+        <div className='header-right'>{linkEls}</div>
+        <div className='header-mobile-icon mobile pointer' onClick={!props.navOpen ? props.toggleNav : () => {}}>
+          <div className='header-icon-line' style={{ transform: props.navOpen ? 'translateY(11.5px) rotate(45deg)' : '' }}></div>
+          <div className='header-icon-line' style={{ width: props.navOpen ? '0' : '' }}></div>
+          <div className='header-icon-line' style={{ transform: props.navOpen ? 'translateY(-11.5px) rotate(-45deg)' : '' }}></div>
+        </div>
+      </header>
+      <div
+        className='header-mobile-nav mobile'
+        style={{
+          transform: props.navOpen ? 'translateY(0)' : 'translateY(-100%)',
+          opacity: props.navOpen ? '0.8' : '0',
+        }}
+      >
+        {linkEls}
+      </div>
+    </div>
+  );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,30 +1,30 @@
-import janePFP from '../assets/jane.jpg'
+import janePFP from '../assets/jane.jpg';
 
 export default function Intro(props) {
-	return (
-		<div className='section' id='Intro'>
-			<div className='intro-content section-limiter'>
-				<img className='intro-img' src={janePFP} />
-				<div className='intro-text'>
-					<h3>
-						Hi, I'm <span style={{ color: 'var(--accent-color)' }}>Jane Casto</span>.
-					</h3>
-					<p>
-						I handle policy analysis, data analytics, and data science, combining the important insights of data with real-world community needs.
-						<br />
-						<br />
-						Come check out what I've been working on recently!
-					</p>
-					<div className='intro-buttons'>
-						<div className='pointer unselectable' onClick={props.sections[2] && props.sections[2].offsetFunc}>
-							Projects
-						</div>
-						<a className='unselectable' href='/Resume.pdf' target='_blank'>
-							<div className='unselectable'>Resume</div>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className='section' id='Intro'>
+      <div className='intro-content section-limiter'>
+        <img className='intro-img' src={janePFP} />
+        <div className='intro-text'>
+          <h3>
+            Hi, I'm <span style={{ color: 'var(--accent-color)' }}>Jane Casto</span>.
+          </h3>
+          <p>
+            I handle policy analysis, data analytics, and data science, combining the important insights of data with real-world community needs.
+            <br />
+            <br />
+            Come check out what I've been working on recently!
+          </p>
+          <div className='intro-buttons'>
+            <div className='pointer unselectable' onClick={props.sections[2] && props.sections[2].offsetFunc}>
+              Projects
+            </div>
+            <a className='unselectable' href='/Resume.pdf' target='_blank'>
+              <div className='unselectable'>Resume</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
